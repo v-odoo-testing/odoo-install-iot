1820b5f169a0bf2368203954e79f2d56ae98aa0a
Danny <danny.goossen@gioxa.com>
2023-07-09 09:07:31 +0700

    make a 3 step

--- instal_dep.sh
+++ instal_dep.sh
@@ -44,34 +44,6 @@ PKGS_TO_INSTALL="
     nginx-full \
     openbox \
     printer-driver-all \
-    python3-cups \
-    python3 \
-    python3-babel \
-    python3-dateutil \
-    python3-decorator \
-    python3-dev \
-    python3-docutils \
-    python3-jinja2 \
-    python3-ldap \
-    python3-libsass \
-    python3-lxml \
-    python3-mako \
-    python3-mock \
-    python3-netifaces \
-    python3-passlib \
-    python3-pil \
-    python3-pip \
-    python3-psutil \
-    python3-psycopg2 \
-    python3-pydot \
-    python3-pypdf2 \
-    python3-qrcode \
-    python3-reportlab \
-    python3-requests \
-    python3-serial \
-    python3-tz \
-    python3-urllib3 \
-    python3-werkzeug \
     rsync \
     screen \
     unclutter \
@@ -81,7 +53,8 @@ PKGS_TO_INSTALL="
     xdotool \
     xserver-xorg-input-evdev \
     xserver-xorg-video-dummy \
-    xserver-xorg-video-fbdev"
+    xserver-xorg-video-fbdev \
+    libusb"
     
 # KEEP OWN CONFIG FILES DURING PACKAGE CONFIGURATION
 # http://serverfault.com/questions/259226/automatically-keep-current-version-of-config-files-when-apt-get-install
@@ -91,35 +64,3 @@ sudo apt-get clean
 #localepurge
 sudo rm -rfv /usr/share/doc
 
-# python-usb in wheezy is too old
-# the latest pyusb from pip does not work either, usb.core.find() never returns
-# this may be fixed with libusb>2:1.0.11-1, but that's the most recent one in raspios
-# so we install the latest pyusb that works with this libusb.
-# Even in stretch, we had an error with langid (but worked otherwise)
-# We fixe the version of evdev to 1.2.0 because in 1.3.0 we have a RuntimeError in 'get_event_loop()'
-PIP_TO_INSTALL="
-    evdev==1.2.0 \
-    gatt \
-    polib \
-    pycups \
-    pyusb \
-    v4l2 \
-    pysmb==1.2.9.1 \
-    cryptocode==0.1"
-
-sudo pip3 install ${PIP_TO_INSTALL}
-
-sudo ln -s /bin/true /bib/tvservice
-
-sudo mkdir -pv /var/log/odoo
-sudo chown pi:pi /var/log/odoo
-
-mkdir -pv /var/run/odoo
-chown pi:pi /var/run/odoo
-
-sudo usermod -a -G lp pi
-
-sudo groupadd usbusers
-sudo usermod -a -G usbusers pi
-
-sudo  systemctl enable --now nginx
--- python3.7.sh
+++ python3.7.sh
@@ -4,15 +4,80 @@ set -o nounset
 set -o pipefail
 
 sudo apt update && sudo apt upgrade
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
+
 wget https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
 tar -xf Python-3.7.13.tar.xz
-sudo mv Python3.7.13 /opt/Python3.7
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
-cd /opt/Python3.7/
+
+
+cd Python-3.7.13
 ./configure --enable-optimizations --enable-shared
-make -j
+make -j8 build_all
 sudo make install
+
+python --version
+
 
 wget https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py
 python3.7 -m pip install --upgrade pip
+
+PIP_TO_INSTALL=" pycups \
+    babel \
+    dateutil \
+    decorator \
+    docutils \
+    jinja2 \
+    ldap \
+    libsass \
+    lxml \
+    mako \
+    mock \
+    netifaces \
+    passlib \
+    pillow \
+    psutil
+    psycopg2 \
+    pydot \
+    pypdf2 \
+    qrcode \
+    reportlab \
+    requests \
+    serial \
+    python3-tz \
+    urllib3 \
+    werkzeug"
+
+sudo pip3 install ${PIP_TO_INSTALL}
+
+# python-usb in wheezy is too old
+# the latest pyusb from pip does not work either, usb.core.find() never returns
+# this may be fixed with libusb>2:1.0.11-1, but that's the most recent one in raspios
+# so we install the latest pyusb that works with this libusb.
+# Even in stretch, we had an error with langid (but worked otherwise)
+# We fixe the version of evdev to 1.2.0 because in 1.3.0 we have a RuntimeError in 'get_event_loop()'
+PIP_TO_INSTALL="
+    evdev==1.2.0 \
+    gatt \
+    polib \
+    pycups \
+    pyusb \
+    pysmb==1.2.9.1 \
+    cryptocode==0.1"
+
+sudo pip3 install ${PIP_TO_INSTALL}
+
+sudo ln -s /bin/true /bib/tvservice
+
+sudo mkdir -pv /var/log/odoo
+sudo chown pi:pi /var/log/odoo
+
+mkdir -pv /var/run/odoo
+chown pi:pi /var/run/odoo
+
+sudo usermod -a -G lp pi
+
+sudo groupadd usbusers
+sudo usermod -a -G usbusers pi
+
+sudo  systemctl enable --now nginx
