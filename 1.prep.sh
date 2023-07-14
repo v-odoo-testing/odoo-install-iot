e1a95fb037e858d9ab41e942b35218676c099b55
Danny <danny@v-consulting.biz>
2023-07-14 09:27:00 +0700

    prep to remove desktop

--- 1.prep.sh
+++ 1.prep.sh
@@ -12,16 +12,24 @@ set -o pipefail
 #export DEBIAN_FRONTEND=noninteractive
 
 # set locale to en_US
-echo "set locale to en_US"
-echo "export LANGUAGE=en_US.UTF-8" >> ~/.bashrc
-echo "export LANG=en_US.UTF-8" >> ~/.bashrc
-echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
-sudo sh -c "echo -e 'en_US.UTF-8 UTF-8\n' > /etc/locale.gen"
-sudo /sbin/locale-gen
+#echo "set locale to en_US"
+#echo "export LANGUAGE=en_US.UTF-8" >> ~/.bashrc
+#echo "export LANG=en_US.UTF-8" >> ~/.bashrc
+#echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
+#sudo sh -c "echo -e 'en_US.UTF-8 UTF-8\n' > /etc/locale.gen"
+#sudo /sbin/locale-gen
 
-source ~/.bashrc
+#source ~/.bashrc
 
+
+#!/usr/bin/env bash
 sudo sh -c "echo 'pi ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
 
-exec bash
+sudo apt-get update && sudo apt-get -y upgrade
+# Do not be too fast to upgrade to more recent firmware and kernel than 4.38
+# Firmware 4.44 seems to prevent the LED mechanism from working
 
+sudo apt-get remove gnome-shell ubuntu-session
+sudo apt-get-autoremove
+
+sudo /sbin/reboot now
